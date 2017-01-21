039f566a676d405b0c56eb502e889376793d5404
vedelopment <vedelopment@gmail.com>
2017-01-20 18:55:32 -0800

    display tools you are able to check out on show path

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -1,4 +1,7 @@
 class ToolsController < ApplicationController
   def show
+
+    @tools = ["power drill", "miter saw", "air compressor", "nail gun", "5-inch rotary sander"]
+
   end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,8 @@ class WelcomeController < ApplicationController
   def index
     @random = Random.new.rand(100)
   end
+
+  def about
+    @language = ["javascript", "html", "css"]
+  end
 end
